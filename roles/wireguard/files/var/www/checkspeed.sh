dabb6ad2fead467726ef86e6fa52e0129d46f86f
Thomas Winter <johnny.bee@gmx.de>
2020-10-06 11:49:16 +0200

    Update checkspeed.sh

--- roles/wireguard/files/var/www/checkspeed.sh
+++ roles/wireguard/files/var/www/checkspeed.sh
@@ -2,6 +2,8 @@
 
 # grober Speedtest. Zuerst stellen wir die aktuelle Zeit in Sekunden fest. Dann laden wir 100MB runter uber VPN und dann stellen wir die neue Zeit fest.....
 t=$(date +"%s"); 
-curl  --interface tun0 https://speed.hetzner.de/100MB.bin -o /dev/null; 
-expr 8 \* 100 / $(($(date +"%s")-$t)) > /var/www/html/speed.txt
-#echo -n "MBit/s: "; expr 8 \* 100 / $(($(date +"%s")-$t))
+curl -k --interface tun0 https://speed.hetzner.de/100MB.bin -o /dev/null; 
+# hat der curl Aufruf funktioniert?
+if [ $? -eq 0 ]; then
+  expr 8 \* 100 / $(($(date +"%s")-$t)) > /var/www/html/speed.txt
+fi
